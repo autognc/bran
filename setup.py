bed1145addb00462a993b2e8deb681107b7e2546
nihaldhamani <nihaldhamani@gmail.com>
2019-03-28 13:05:26 -0500

    adding setup script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='bran',
+    version='0.0.1',
+    description='EC2 instance creation CLI',
+    packages=['bran'],
+    install_requires=[
+        'boto3>=1.9.104',
+        'botocore>=1.12.104',
+        'boto>=2.49.0',
+        'pyinquirer>=1.0.3',
+        'pyperclip>=1.7.0',
+        'progress>=1.4'
+    ],
+    entry_points='''
+      [console_scripts]
+      bran=bran.bran:main
+    ''',
+)
