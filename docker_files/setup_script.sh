4fcde7bae977d1a145b0a038ce6b4559832bc906
Nihal Dhamani <nihaldhamani@gmail.com>
2019-06-24 19:13:31 -0700

    update setup script to handle external plugins directory

--- docker_files/setup_script.sh
+++ docker_files/setup_script.sh
@@ -4,6 +4,7 @@ set -e
 apt-get update
 apt-get -y install git
 git clone https://github.com/autognc/ravenML.git
+git clone https://github.com/autognc/ravenML-plugins.git
 #apt-get -y install python3.6
 apt-get -y install python3-pip
 update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
@@ -12,12 +13,12 @@ update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 #source ~/.bashrc
 pip install --upgrade setuptools
 cd ravenML
-git checkout bbox_plugin_dev
+#git checkout bbox_plugin_dev
 pip install -r requirements.txt
 pip install -e .
 pip install halo
 pip install pyyaml
-cd plugins
+cd ../ravenML-plugins
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 ./install_all.sh -c
