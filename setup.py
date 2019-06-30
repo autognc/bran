2ebf70bccd5db2632136d6632322396de965152b
Nihal Dhamani <nihaldhamani@gmail.com>
2019-06-29 18:17:24 -0700

    v0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bran',
-    version='0.0.6',
+    version='0.1.0',
     description='EC2 instance creation CLI',
     packages=['bran'],
     install_requires=[
